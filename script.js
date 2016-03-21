6d214bba8deca2633491629beb7c2b5aebe42bd2
Andre Askarinam <andreaskarinam@Andres-MacBook-Pro-2.local>
2016-03-21 11:06:46 -0700

    Added functionality

--- script.js
+++ script.js
@@ -8,12 +8,24 @@ $(document).ready(function() {
 	$(document).click(function() {
     	var img = $('<img src="Icons/andre.png"/>');
     	// var img = $('<img src="Icons/andre.png" class="andre_img" />');
+
+  //   	var img = $("img")[0]; // Get my img elem
+		// var pic_real_width, pic_real_height;
+		// $("<img/>") // Make in memory copy of image to avoid css issues
+		//     .attr("src", $(img).attr("src"))
+		//     .load(function() {
+		//         pic_real_width = this.width;   // Note: $(this).width() will not
+		//         pic_real_height = this.height; // work for in memory images.
+		//     });
     	img.addClass()
     	var window_height = $(window).height();
     	var window_width = $(window).width();
-    	var top_loc = (window_height * Math.random()).toFixed();
+    	var top_loc = (window_height * Math.random() - img.).toFixed();
     	var left_loc = (window_width * Math.random()).toFixed();
-    	img.css({top: top_loc, left: left_loc});
+    	console.log(top_loc + " " + left_loc);
+    	// img.css({top: top_loc, left: left_loc});
+    	$(img).parent().css({position: 'relative'});
+		$(img).css({top: top_loc + "px", left: left_loc + "px", position:'absolute'});
     	$('div').append(img);
 	});
 });
